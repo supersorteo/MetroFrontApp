769dc58d63b00feca632aaa218a901f6dc71effc
leodanis <leodanismiranda@gmail.com>
2025-08-22 05:46:41 -0400

    guardarrr

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,24 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr';
+import { provideServiceWorker } from '@angular/service-worker';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
     provideAnimations(),
-    provideToastr({timeOut: 900, preventDuplicates: true})
-
-  ]
+    provideToastr({ timeOut: 900, preventDuplicates: true }),
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    }),
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+]
 };
